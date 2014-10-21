83a3441f12763308aed56d31c2f69aecc2c558d9
Pelayo Parejo Pagador <pelayo.parejopagador@epitech.eu>
2014-10-21 18:44:11 +0000

    simple_mangling

--- mangler/simple_mangling.py
+++ mangler/simple_mangling.py
@@ -1,34 +1,5 @@
 from cnorm.nodes import Signs
-
 S = Signs.map
-
-"""
-Method = 'method ident'  '__' 'qual' 'classident' 'params'
-
-Primitive types:
-int, long, short, char, long long = 'i', 'l', 's', 'c', 'x'
-if unsigned = prefixed 'U'
-signed char = 'Sc'
-float, double = 'f', 'd'
-bool = 'b'
-wchar_t = 'w'
-
-Simple ident:
-class, template, identspace ident = 'len(ident)' 'ident'
-
-Pointer = 'P' 'type mangling'
-Reference = 'R' 'type mangling'
-
-Qualified ident = 'Q' '# of parts in qualified ident'
-if 9 or less: No delimiters
-Else: '_' before and after count
-
-Foo::bar(int, long) const is mangled as `bar__C3Fooil
-
-TODO:
-Unicode support
-http://www.ofb.net/gnu/gcc/gxxint_15.html
-"""
 
 VARS = {
    'char':'c',
@@ -40,24 +11,19 @@ VARS = {
    'signed char':'Sc'
 }
 
-QUAL = {
-   'const':'C'
-   }
+def id_m(ident: str):
+    return str(len(ident)) + ident
 
-def identifier(ident: str):
-     return str(len(ident)) + ident
-
-def type(ident: str, typ: str, qual: str = None, sign = S['SIGNED']):
+def type_m(typ: str, sign = S['SIGNED']):
   if typ not in VARS:
      raise IndexError('Mangling type not recognised')
   res = ''
   if sign == S['UNSIGNED']:
      res = 'U'
-  res += VARS[typ] + identifier(ident)
+  res += VARS[typ]
   return res
 
-def qualifier():
-    pass
-
-def function(ident: str, scope: str = '', qual: str = '', params: list = None):
-   return  '__' + qual + self.ident(ident) + ''.join(params)
+def qual_m(qual: str):
+  if qual == '':
+    raise IndexError('Qual size should not be empty')
+  return qual[0].upper() + '_'
