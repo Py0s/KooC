7ca95496be66983f2043f6d6f2cb37d90b8bbb77
lesaff_r <remi.lesaffre@epitech.eu>
2014-10-21 17:18:34 +0200

    ajout gestion pointeurs dans le typage. Mangling foireux fait main pour tester touts les params recuperes. Nom plus explicits dans Module. Ajout de nouveaux tests dans les test/muf*

--- KoocGrammar/Module.py
+++ KoocGrammar/Module.py
@@ -8,14 +8,14 @@ from KoocGrammar.KC_Statement import KC_Statement
 class   Module(Grammar, KC_Statement):
     entry = 'Module'
     grammar = """
-                module = [ "@module" Module.Name:name KC_Statement.kc_statement:body #Mod(current_block, name, body) ]
+                module = [ "@module" Module.Name:module_name KC_Statement.kc_statement:body #Mod(current_block, module_name, body) ]
 
                 Name = [ [['a'..'z']|['A'..'Z']]+ ]
               """
 
 
 @meta.hook(Module)
-def Mod(self, ast, name, body):
+def Mod(self, ast, module_name, body):
     if hasattr(body, "body") and body.body:
         for item in body.body:
             if (hasattr(item, "_ctype") and hasattr(item._ctype, "_storage")):
