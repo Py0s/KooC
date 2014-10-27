19a8c511bcf2015b54bcf31e511db16ff2bc8293
Pelayo <parejo_p@epitech.eu>
2014-10-27 10:57:24 +0100

    added mangle init file

--- mangler/mangle.py
+++ mangler/mangle.py
@@ -1,6 +1,54 @@
 from pyrser import meta
 import knodes
 
+@meta.add_method(knodes.Module)
+def mangle(self):
+    pass
+
 @meta.add_method(knodes.Class)
 def mangle(self):
     self._identifier = 'K' + simple_mangling.identifier(self._identifier)
+
+@meta.add_method(knodes.KDeclType)
+def mangle(self):
+    pass
+
+@meta.add_method(knodes.KPointerType)
+def mangle(self):
+    pass
+
+@meta.add_method(knodes.KArrayType)
+def mangle(self):
+    pass
+
+@meta.add_method(knodes.KParenType)
+def mangle(self):
+    pass
+
+@meta.add_method(knodes.KQualType)
+def mangle(self):
+    pass
+
+@meta.add_method(knodes.KAttrType)
+def mangle(self):
+    pass
+
+@meta.add_method(knodes.KType)
+def mangle(self):
+    pass
+
+@meta.add_method(knodes.KPrimaryType)
+def mangle(self):
+    pass
+
+@meta.add_method(knodes.KComposedType)
+def mangle(self):
+    pass
+
+@meta.add_method(knodes.KFuncType)
+def mangle(self):
+    pass
+
+@meta.add_method(knodes.KDecl)
+def mangle(self):
+    pass
