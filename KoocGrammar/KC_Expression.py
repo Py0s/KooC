f8b5b9c1377e5ec3d8e7ee643c07270ab5400218
marie <marie.lefebvre@epitech.eu>
2014-10-28 11:55:48 +0100

    surcharge des bnf expression et statement comme des petits porcinets

--- KoocGrammar/KC_Expression.py
+++ KoocGrammar/KC_Expression.py
@@ -3,17 +3,283 @@ from pyrser.grammar import Grammar
 from cnorm.parsing.expression import Expression
 from KoocGrammar.K_Expression import K_Expression
 
-class KC_Expression(Grammar, Expression, K_Expression):
-    entry = "unary_expression"
-    grammar = """
-        kc_expression = [ Expression.expression:>_ ]
-        kc_assignement_expression = [ Expression.assignement_expression:>_ ]
-        unary_expression = [ Expression.unary_expression:>_ ]
-        primary_expression = [
-            '(' expression:expr ')' #new_paren(_, expr)
-            | [ Literal.literal | identifier | kc_primary_expression]:>_
-        ]
-        kc_primary_expression = [ K_Expression.k_primary_expression:>_ ]
+# class KC_Expression(Grammar, Expression, K_Expression):
+#     entry = "unary_expression"
+#     grammar = """
+#         kc_expression = [ Expression.expression:>_ ]
+#         kc_assignement_expression = [ Expression.assignement_expression:>_ ]
+#         unary_expression = [ Expression.unary_expression:>_ ]
+#         primary_expression = [
+#             '(' expression:expr ')' #new_paren(_, expr)
+#             | [ Literal.literal | identifier | kc_primary_expression]:>_
+#         ]
+#         kc_primary_expression = [ K_Expression.k_primary_expression:>_ ]
 
-        K_Expression.assmt_expr_overide = [ KC_Expression.kc_assignement_expression:>_ ]
+#         K_Expression.assmt_expr_overide = [ KC_Expression.kc_assignement_expression:>_ ]
+#     """
+
+
+class KC_Expression(Grammar, Expression, K_Expression):
+    """
+        interaction with other CNORM PART:
+
+    """
+    entry = "kc_expression"
+    grammar = """
+
+        /*
+            Comment works as in C/C++
+        */
+
+        kc_dummy_with_brace = [ @ignore('null')
+            [
+            '{' kc_dummy_with_brace* '}'
+            | Base.string
+            | Base.char
+            | Base.read_char:c #check_not_brace(c)
+            ]
+        ]
+
+        kc_dummy_with_paren = [ @ignore('null')
+            [
+            '(' kc_dummy_with_paren* ')'
+            | Base.string
+            | Base.char
+            | Base.read_char:c #check_not_paren(c)
+            ]
+        ]
+
+        kc_expression = [
+            kc_assignement_expression:>_
+            [
+                ',':op #new_raw(op, op)
+                kc_assignement_expression:param
+                #new_binary(_, op, param)
+            ]*
+        ]
+
+        kc_assign_op = [
+            @ignore('null')
+            [
+                '=' !'='
+                | "+="
+                | "-"
+                | "*="
+                | "/="
+                | "%="
+                | "<<="
+                | ">>="
+                | "&="
+                | "^="
+                | "|="
+            ]:op
+            #new_raw(_, op)
+        ]
+        kc_assignement_expression = [
+            kc_conditional_expression:>_
+            [
+                kc_assign_op:op
+                kc_assignement_expression:param
+                #new_binary(_, op, param)
+            ]*
+        ]
+
+        kc_constant_expression = [ kc_conditional_expression:>_ ]
+
+        kc_conditional_expression = [
+            kc_logical_or_expression:>_
+            [
+                '?'
+                kc_expression?:then
+                ':'
+                kc_assignement_expression?:else
+                #new_ternary(_, then, else)
+            ]?
+        ]
+
+        kc_logical_or_op = [ @ignore('null') ["||"]:op #new_raw(_, op) ]
+        kc_logical_or_expression = [
+            kc_logical_and_expression:>_
+            [
+                kc_logical_or_op:op
+                kc_logical_and_expression:param
+                #new_binary(_, op, param)
+            ]*
+        ]
+
+        kc_logical_and_op = [ @ignore('null') ["&&"]:op #new_raw(_, op) ]
+        kc_logical_and_expression = [
+            kc_or_expression:>_
+            [
+                kc_logical_and_op:op
+                kc_or_expression:param
+                #new_binary(_, op, param)
+            ]*
+        ]
+
+        kc_or_op = [ @ignore('null') ["|" !["|"|"="]]:op #new_raw(_, op) ]
+        kc_or_expression = [
+            kc_xor_expression:>_
+            [
+                kc_or_op:op
+                kc_xor_expression:param
+                #new_binary(_, op, param)
+            ]*
+        ]
+
+        xor_op = [ @ignore('null') ["^" !"="]:op #new_raw(_, op) ]
+        kc_xor_expression = [
+            and_expression:>_
+            [
+                xor_op:op
+                and_expression:param
+                #new_binary(_, op, param)
+            ]*
+        ]
+
+        kc_and_op = [ @ignore('null') ["&" !["&"|"="]]:op #new_raw(_, op) ]
+        kc_and_expression = [
+            kc_equality_expression:>_
+            [
+                kc_and_op:op
+                kc_equality_expression:param
+                #new_binary(_, op, param)
+            ]*
+        ]
+
+        kc_eqneq_op = [ @ignore('null') ["==" | "!="]:op #new_raw(_, op) ]
+        kc_equality_expression = [
+            kc_relational_expression:>_
+            [
+                kc_eqneq_op:op
+                kc_relational_expression:param
+                #new_binary(_, op, param)
+            ]*
+        ]
+
+        kc_cmp_op = [
+            @ignore('null')
+            [
+                "<="
+                | ">="
+                | '<' !'<'
+                | '>' !'>'
+            ]:op
+            #new_raw(_, op)
+        ]
+        kc_relational_expression = [
+            kc_shift_expression:>_
+            [
+                kc_cmp_op:op
+                kc_shift_expression:param
+                #new_binary(_, op, param)
+            ]*
+        ]
+
+        kc_shift_op = [
+            @ignore('null')
+            [
+                "<<" !"="
+                | ">>" !"="
+            ]:op
+            #new_raw(_, op)
+        ]
+        kc_shift_expression = [
+            kc_additive_expression:>_
+            [
+                kc_shift_op:op
+                kc_additive_expression:param
+                #new_binary(_, op, param)
+            ]*
+        ]
+
+        kc_add_op = [
+            @ignore('null')
+            [
+                '+' !['+'|'=']
+                | '-' !['-'|'='|'>']
+            ]:op
+            #new_raw(_, op)
+        ]
+        kc_additive_expression = [
+            kc_multiplicative_expression:>_
+            [
+                kc_add_op:op
+                kc_multiplicative_expression:param
+                #new_binary(_, op, param)
+            ]*
+        ]
+
+        kc_mul_op = [ @ignore('null') [['*'|'/'|'%']:op !'='] #new_raw(_, op) ]
+        kc_multiplicative_expression = [
+            kc_unary_expression:>_
+            [
+                kc_mul_op:op
+                kc_unary_expression:param
+                #new_binary(_, op, param)
+            ]*
+        ]
+
+        kc_unary_op = [ @ignore('null')
+            ["++"
+            |"--"
+            |"&&"
+            | '&' !'='
+            | '*' !'='
+            | '~' !'='
+            | '!' !'='
+            | '+' !'='
+            | '-' !['>'|'=']
+            ]:op #new_raw(_, op)
+        ]
+        kc_unary_expression =
+        [
+                kc_postfix_expression:>_
+            |
+                __scope__:op
+                [   kc_unary_op:>op
+                |   Base.id:i
+                    #is_raw(op, i)
+                ]
+                kc_unary_expression:expr
+                #new_unary(_, op, expr)
+        ]
+
+        kc_postfix_expression = [
+            kc_primary_expression:>_
+            [
+                __scope__:pres
+                [
+                '[' kc_expression:expr ']' #new_array_call(pres, _, expr)
+                | '(' func_arg_list?:args ')' #new_func_call(pres, _, args)
+                | '.' kc_identifier:i #new_dot(pres, _, i)
+                | "->" kc_identifier:i #new_arrow(pres, _, i)
+                | ["++"|"--"]:op #new_raw(op, op) #new_post(pres, op, _)
+                ]
+                #bind('_', pres)
+            ]*
+        ]
+
+        func_arg_list = [
+            kc_assignement_expression:a #new_arg(_, a)
+            [   ','
+                kc_assignement_expression:a #new_arg(_, a)
+            ]*
+        ]
+
+        kc_primary_expression = [
+            '(' kc_expression:expr ')' #new_paren(_, expr)
+            | [ Literal.literal | kc_identifier ]:>_
+        ]
+
+        kc_identifier = [
+            @ignore('null')
+            [
+                kc_rootidentifier:id
+                #check_is_id(id)
+                #new_id(_, id)
+            ]
+        ]
+
+        kc_rootidentifier = [ Base.id ]
     """
--- KoocGrammar/KC_Statement.py
+++ KoocGrammar/KC_Statement.py
@@ -4,8 +4,134 @@ from cnorm.parsing.statement import Statement
 from KoocGrammar.K_Statement import K_Statement
 from pyrser import meta
 
+# class KC_Statement(Grammar, Statement, K_Statement):
+#     entry = "expression_statement"
+#     grammar = """
+#     kc_statement = [ Statement.single_statement:>_ ]
+#     """
+
 class KC_Statement(Grammar, Statement, K_Statement):
-    entry = "expression_statement"
+    """
+        interaction with other CNORM PART:
+
+        Declaration.init_declarator -> kc_compound_statement
+        Expression.primary_expression -> block_item_listC_Sta
+    """
+    entry = "kc_single_statement"
     grammar = """
-    kc_statement = [ Statement.single_statement:>_ ]
+
+        /*
+            Comment works as in C/C++
+        */
+
+        kc_single_statement = [
+            [kc_compound_statement
+            | kc_labeled_statement
+            | kc_expression_statement
+            ]:>_
+        ]
+
+        kc_compound_statement = [
+            [
+            '{'
+                __scope__:current_block
+                #new_blockstmt(_, current_block)
+                [
+                    kc_line_of_code
+                ]*
+            '}'
+            ]
+        ]
+
+        kc_line_of_code = [
+            kc_single_statement:line
+            #end_loc(current_block, line)
+        ]
+
+        kc_labeled_statement = [
+            KC_Expression.kc_rootidentifier:ident
+            [ #check_stmt(ident, "if") kc_if_statement:>_
+            | #check_stmt(ident, "for") kc_for_statement:>_
+            | #check_stmt(ident, "while") kc_while_statement:>_
+            | #check_stmt(ident, "switch") kc_switch_statement:>_
+            | #check_stmt(ident, "do") kc_do_statement:>_
+            | #check_stmt(ident, "return") kc_return_statement:>_
+            | #check_stmt(ident, "goto") kc_goto_statement:>_
+            | #check_stmt(ident, "case") kc_case_statement:>_
+            | #check_stmt(ident, "break") ';' #new_break(_)
+            | #check_stmt(ident, "continue") ';' #new_continue(_)
+            | ':' #new_label(_, ident)
+            ]
+        ]
+
+        kc_if_statement = [
+            '(' kc_expression:cond ')'
+            kc_single_statement:then
+            __scope__:else
+            [
+                "else"
+                kc_single_statement:>else
+            ]?
+            #new_if(_, cond, then, else)
+        ]
+
+        kc_for_statement = [
+            '('
+                kc_expression_statement:init
+                kc_expression_statement:cond
+                kc_expression?:inc
+            ')'
+            kc_single_statement:body
+            #new_for(_, init, cond, inc, body)
+        ]
+
+        kc_while_statement = [
+            '('
+                kc_expression:cond
+            ')'
+            kc_single_statement:body
+            #new_while(_, cond, body)
+        ]
+
+        kc_switch_statement = [
+            '(' kc_expression:cond ')'
+            kc_single_statement:body
+            #new_switch(_, cond, body)
+        ]
+
+        kc_do_statement = [
+            kc_single_statement:body
+            "while" '(' kc_expression:cond ')' ';'
+            #new_do(_, cond, body)
+        ]
+
+        kc_return_statement = [
+            kc_expression?:e ';'
+            #new_return(_, e)
+        ]
+
+        kc_goto_statement = [
+            kc_expression:e ';'
+            #new_goto(_, e)
+        ]
+
+        kc_range_expression = [
+            kc_constant_expression:>_
+            [
+                "..."
+                kc_constant_expression:r
+                #new_range(_, r)
+            ]?
+        ]
+
+        kc_case_statement = [
+            kc_range_expression:e #new_case(_, e)
+            ':'
+        ]
+
+        kc_expression_statement = [
+            [kc_expression:e #new_expr(_, e)]?
+            ';'
+        ]
+
     """
