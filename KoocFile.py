749e7a77884c5549b1e3514a8024036ee6ef3aa0
marie <marie.lefebvre@epitech.eu>
2014-11-06 22:14:44 +0100

    bonne partie de l'implementation de KoocFile

--- KoocFile.py
+++ KoocFile.py
@@ -21,26 +21,52 @@ class KFImpl():
     def __init__(self):
         pass
 
-    def is_var_in_module(module_name, symbol_name, symbol_type):
+    def params_types_equals(first, second):
+        # TODO !
+        return True
+
+    def symbol_entry_in_module(module_name, symbol_name):
+        return modules[module_name][symbol_name]
+    def symbol_entry_in_classe(classe_name, symbol_name):
+        return classes[classe_name][symbol_name]
+
+    def symbol_overload(symbol_entry, symbol_type, params_types=[]):
+        for overload in symbol_entry:
+            if overload[0] == symbol_type and params_types_equals(overload[1], params_types):
+                return overload
+        return None
+
+    def is_var_in_module(module_name, symbol_name, symbol_type, params_types=[]):
         if module_name in modules\
-            and symbol_name in modules[module_name]\
-                and symbol_type in modules[module_name][symbol_name]:
+            and symbol_name in modules[module_name]:
+                for overload in modules[module_name][symbol_name]:
+                    if overload[0] == symbol_type and params_types_equals(overload[1], params_types):
                         return True
         return False
     
-    def get_var_from_module(module_name, symbol_name, symbol_type):
-        if not is_var_in_module(module_name, symbol_name, symbol_type):
+    def get_var_from_module(module_name, symbol_name, symbol_type, params_types=[]):
+        if not is_var_in_module(module_name, symbol_name, symbol_type, params_types):
             raise RuntimeError("try to get unknown node")
-        return modules[module_name][symbol_name][symbol_type]
+        symbol_entry = modules[module_name][symbol_name]
+        return symbol_overload(symbol_entry, symbol_type, params_types)
 
+    def get_var_for_symbol(module_name, symbol_name, symbol_type, params_types=[]):
+        var = None
+        if is_var_in_module(module_name, symbol_name, symbol_type, params_types):
+            if is_var_in_class(module_name, symbol_name, symbol_type, params_types):
+                raise RuntimeError("Unbiguous call")#TODO : jolie msg
+            else:
+                var = get_var_from_module(module_name, symbol_name, symbol_type, params_types)
+        elif is_var_in_class(module_name, symbol_name, symbol_type, params_types):
+            var = get_var_from_module(module_name, symbol_name, symbol_type, params_types)
+        else:
+            raise RuntimeError("No symbol")#TODO : jolie msg
+        return var
 
 def is_file_imported(fileName):
     return fileName in imported_file_names
 def register_file(file_name):
-    if is_file_imported(file_name):
-        return False #msg d'erreur ?
     imported_file_names.append(file_name)
-    return True
 
 
 def register_module(module_name):
@@ -52,54 +78,35 @@ def register_class(class_name):
     else:
         raise RuntimeError("Redefinition of class \"" + class_name + "\"")
 
-def register_module_symbol(module_name, symbol_name, symbol_type, mangled_name, assign_node = None):
+def register_module_symbol(module_name, symbol_name, symbol_type, mangled_name, params_types=[], assign_node = None):
+    # verifier l'existence du module ?
     if not symbol_name in modules[module_name]:
         modules[module_name][symbol_name] = []
     symbol_entry = modules[module_name][symbol_name]
-    symbol_entry.append((symbol_type, mangled_name, assign_node))   
-    # print("MODULE !! ", modules)
-    #checker si cette surcharge existe deja, dans ce cas -> erreur
-    # '-> appeler is_var_in_module
+    for symbol_overload in symbol_entry:
+        if symbol_overload[1] == mangled_name:
+            raise RuntimeError("Redefinition of symbol \"" + symbol_name + "\" in the module \"" + module_name + "\"")
+    symbol_entry.append((symbol_type, params_types, mangled_name, assign_node))
     
-def register_class_symbol(module_name, symbol_name, symbol_type, mangled_name, assign_node = None):
+def register_class_symbol(module_name, symbol_name, symbol_type, mangled_name, params_types=[], assign_node = None):
     # TODO !
     pass
 
 # def register_class_symbols(class_name, symbols)
 #    pass
 
-def mangled_name_of_symbol(module_name, symbol_name, symbol_type):
-    return KFImpl.get_var_from_module()[0]
-def assign_node_of_symbol(module_name, symbol_name, symbol_type):
-    return KFImpl.get_var_from_module()[1]
 
+# ! ici module_name signifie aussi bien nom du module que de la classe
+def mangled_name_of_symbol(module_name, symbol_name, symbol_type, params_types=[]):
+    return KFImpl.get_var_for_symbol(module_name, symbol_name, symbol_type, params_types)[2]
+def assign_node_of_symbol(module_name, symbol_name, symbol_type, params_types=[]):
+    return KFImpl.get_var_for_symbol(module_name, symbol_name, symbol_type, params_types)[3]
+
+
+# TODO : ne retourner que la vtable ou toutes les fonctions membres ?
 def symbols_of_class(class_name):
     if not class_name in classes:
         raise RuntimeError("No definition of class \"" + class_name + "\"")
     return classes[class_name]
 
 
-
-
-# def register_class(className):
-#     classes.append(className)
-# def register_module(moduleName):
-#     modules.append(moduleName)
-
-# def register_class_function(className, mangled_name):
-#     classes[className].append(mangled_name)
-# def register_module_function(moduleName, mangled_name):
-#     modules[moduleName].append(mangled_name)
-
-# def is_module_function(mangled_name):
-#     for module in modules:
-#         for symbol in module:
-#             if symbol == mangled_name:
-#                 return True
-#     return False
-# def is_class_function(mangled_name):
-#     for classe in classes:
-#         for symbol in classe:
-#             if symbol == mangled_name:
-#                 return True
-#     return False
