25e46ad50d9d11e4bbae929997e9dd1e98282314
lesaff_r <remi.lesaffre@epitech.eu>
2014-11-08 23:50:25 +0100

    Ligne vide supprimée de K_Expression. Setup de Kooc_call pour appel a KoocFile ATTENTION:PROBLEME AVEC [MODULE.VAR]! Modif des class_test.kc/kh pour plus de coerence. Ajout d'une variable en plus dans muf.kh pour les tests. Modif du fichier de tests unitaires pour le module (test_module), non fini, chose a faire en TODO commenté

--- KoocGrammar/Kooc_call.py
+++ KoocGrammar/Kooc_call.py
@@ -9,17 +9,19 @@ class   Kooc_call(Grammar):
     grammar = """
     kooc_call              = [
                                 __scope__:type
-                                ["@!(" kooc_type:type ")"]? "["
+                                ["@!(" kooc_type:type ")"]? '[' #echo("ET OUAIS!")
                                     module_id:module
-                                    [[ function_id:func list_parameter:params #create_func_symbol(_, module, type, func, params)]
-                                    |
-                                    '.' variable_id:var #create_var_symbol(_, module, type, var) ]
+                                    [
+                                     [function_id:func list_parameter:params #create_func_symbol(_, module, type, func, params)]
+                                     |
+                                     ['.' variable_id:var #create_var_symbol(_, module, type, var)]
+                                    ]
                                 ']'
                              ]
     kooc_type              = [ ['a'..'z'|'A'..'Z'|'*']* ]
     module_id              = [ Base.id ]
     function_id            = [ Base.id ]
-    list_parameter         = [ #create_params(_) [':' ["(" kooc_type:type ")"]? assmt_expr_overide:param #save_param(_, type, param)]* ]
+    list_parameter         = [ #create_params(_) [':' ["(" kooc_type:type ")"] assmt_expr_overide:param #save_param(_, type, param)]* ]
     variable_id            = [ Base.id ]
 
 
@@ -29,37 +31,42 @@ class   Kooc_call(Grammar):
     assmt_expr_overide     = [ "regle overide dans KC_Expression" ]
     """
 
-
-## class Class(nodes.Func):
-##     def __init__(self, call_expr, params):
-##         super().__init__(call_expr, params)
-
-##     def to_c(self):
-##         print("Tuturu~")
-##         res = super().to_c()
-##         print(res)
-##         return res
+# @meta.hook(Kooc_call)
+# def test_ouais(self, ast):
+#     print(self._stream.peek_char)
+#     print("OUAIS?")
+#     return True
 
 @meta.hook(Kooc_call)
 def create_params(self, ast):
     ast.params = []
+    ast.types = []
     return True
 
 @meta.hook(Kooc_call)
 def save_param(self, ast, typo, param):
     if type(param) is nodes.Func:
         ast.params.append(param)
+        ast.types.append(self.value(typo))
     else:
-        ast.params.append(nodes.Literal(self.value(typo) + "_" + self.value(param)))
+        ast.params.append(nodes.Literal(self.value(param)))
+        ast.types.append(self.value(typo))
     return True
 
 @meta.hook(Kooc_call)
 def create_func_symbol(self, ast, module, typo, func, params):
-##    ast.set(Class(nodes.Id(self.value(module) + self.value(func)), params.params))
+    # print("Module : ", self.value(module))
+    # print("Type retour : ", self.value(typo))
+    # print("Nom fonction :", self.value(func))
+    # print("Types params : ", params.types)
+    # print("Params : ", params.params)
+    # print("")
     ast.set(nodes.Func(nodes.Id(self.value(module) + "_" + self.value(typo) + "_" + self.value(func)), params.params))
     return True
 
 @meta.hook(Kooc_call)
 def create_var_symbol(self, ast, module, typo, var):
+    # print("Type variable :", self.value(typo))
+    # print("")
     ast.set(nodes.Id(self.value(module) + "_" + self.value(typo) + "_" + self.value(var)))
     return True
--- unittests/test_module.py
+++ unittests/test_module.py
@@ -6,60 +6,136 @@ from KoocGrammar import Module
 from cnorm.passes import to_c
 
 class UnittestModule(unittest.TestCase):
-    pass
 
-    # def setUp(self):
-    #     self.cparse = KoocG()
+    def setUp(self):
+        self.cparse = KoocG()
 
-    # def test_empty_module(self):
-    #     res = str(self.cparse.parse(
-    #         """
-    #         @module Test
-    #         {
-    #         }
-    #         """).to_c())
-    #     self.assertEqual(res,
-    #                      "")
-    # def test_simple_declaration_variable(self):
-    #     res = str(self.cparse.parse(
-    #         """
-    #         @module Test
-    #         {
-    #          void test;
-    #         }
-    #         """).to_c())
-    #     self.assertEqual(res,
-    #                      "")
-    # def test_simple_declaration_assignement_variable(self):
-    #     res = str(self.cparse.parse(
-    #         """
-    #         @module Test
-    #         {
-    #          int test = 42;
-    #         }
-    #         """).to_c())
-    #     self.assertEqual(res,
-    #                      "")
-    # def test_simple_declaration_function(self):
-    #     res = str(self.cparse.parse(
-    #         """
-    #         @module Test
-    #         {
-    #          void test();
-    #         }
-    #         """).to_c())
-    #     self.assertEqual(res,
-    #                      "")
-    # def test_declaration_function(self):
-    #     res = str(self.cparse.parse(
-    #         """
-    #         @module Test
-    #         {
-    #          char *test(int **toto, float tata[]);
-    #         }
-    #         """).to_c())
-    #     self.assertEqual(res,
-    #                     "")
+    ## SIMPLE TO MORE COMPLEX TEST OF VALIDS MODULES
+    def test_empty_module(self):
+        res = str(self.cparse.parse(
+            """
+            @module Test
+            {
+            }
+            """).to_c())
+        self.assertEqual(res,
+                         "")
+    def test_declaration_variable(self):
+        res = str(self.cparse.parse(
+            """
+            @module Test
+            {
+             void test;
+            }
+            """).to_c())
+        self.assertEqual(res,
+                         "extern void M4Test__v4test;\n")
+    def test_declaration_assignement_variable(self):
+        res = str(self.cparse.parse(
+            """
+            @module Test
+            {
+             int test = 42;
+            }
+            """).to_c())
+        self.assertEqual(res,
+                         "extern int M4Test__i4test;\n")
+    def test_declaration_function_implicit_void(self):
+        res = str(self.cparse.parse(
+            """
+            @module Test
+            {
+             void test();
+            }
+            """).to_c())
+        self.assertEqual(res,
+                         "extern void M4Test__v4testv();\n")
+    def test_declaration_function_explicit_void(self):
+        res = str(self.cparse.parse(
+            """
+            @module Test
+            {
+             void test(void);
+            }
+            """).to_c())
+        self.assertEqual(res,
+                         "extern void M4Test__v4testv(void);\n")
+    def test_declaration_function(self):
+        res = str(self.cparse.parse(
+            """
+            @module Test
+            {
+             char *test(int **toto, float tata[]);
+            }
+            """).to_c())
+        self.assertEqual(res,
+                        "extern char *M4Test__Pc4testPPiAf(int **toto, float tata[]);\n")
+    def test_variable_overload(self):
+        res = str(self.cparse.parse(
+            """
+            @module Mayuri
+            {
+                int tuturu;
+                float tuturu;
+            }
+            """).to_c())
+        self.assertEqual(res,
+                         "extern int M6Mayuri__i6tuturu;\nextern float M6Mayuri__f6tuturu;\n")
+    def test_variable_and_function_with_no_param(self):
+        res = str(self.cparse.parse(
+            """
+            @module Mayuri
+            {
+                int tuturu;
+                int tuturu();
+            }
+            """).to_c())
+        self.assertEqual(res,
+                         "extern int M6Mayuri__i6tuturu;\nextern int M6Mayuri__i6tuturuv();\n")
+    def test_function_return_value_overload(self):
+        res = str(self.cparse.parse(
+            """
+            @module Mayuri
+            {
+                int tuturu(float toto);
+                float tuturu(float tutu);
+            }
+            """).to_c())
+        self.assertEqual(res,
+                         "extern int M6Mayuri__i6tuturuf(float toto);\n\
+extern float M6Mayuri__f6tuturuf(float tutu);\n")
+    def test_function_params_value_overload(self):
+        res = str(self.cparse.parse(
+            """
+            @module Mayuri
+            {
+                double **tuturu(char toto[], void* ptr[]);
+                double** tuturu(int tutu);
+            }
+            """).to_c())
+        self.assertEqual(res,
+                         "extern double **M6Mayuri__PPd6tuturuAcAPv(char toto[], void *ptr[]);\n\
+extern double **M6Mayuri__PPd6tuturui(int tutu);\n")
+
+    ## TODO : TESTS WITH SOME FUNCTION POINTER
+
+    ## TEST OF OVERLOADS WITH DIFFERENTS STORAGES, QUALIFIERS OR SPECIFIER
+    # Little reminder :
+    # Storages = meta.enum('AUTO', 'REGISTER', 'TYPEDEF',
+    #                  'STATIC', 'EXTERN', 'INLINE',
+    #                  'VIRTUAL', 'EXPLICIT',
+    #                  'FORCEINLINE', 'THREAD')
+    # Qualifiers = meta.enum('AUTO', 'CONST', 'VOLATILE', 'RESTRICT',
+    #                    'W64', 'STDCALL', 'CDECL',
+    #                    'PTR32', 'PTR64', 'FASTCALL')
+    # Specifiers = meta.enum('AUTO', 'STRUCT', 'UNION', 'ENUM', 'LONG',
+    #                    'LONGLONG', 'SHORT')
+
+    # TODO!!
+
+    ## TEST OF INVALIDS MODULES?
+
+    # TODO!!?
 
     ## TODO : A mettre dans test_implementation
     ## def test_simple_koocCall_in_function(self):
