54353879ad663f395dce6bba056ab52f533a3f69
lesaff_r <remi.lesaffre@epitech.eu>
2014-11-08 17:53:50 +0100

    gestion type modifiée dans KoocFile Impl + Gestion sauvegarde/récup->Module/Implementation (pour le module) OK. ATTENTION : le to_c vient tout peter et pas de catch de l exception si symbole non existant

--- KoocGrammar/Implementation.py
+++ KoocGrammar/Implementation.py
@@ -4,6 +4,7 @@ from pyrser.grammar import Grammar
 from pyrser import meta
 from KoocGrammar.KC_Statement import KC_Statement
 import KoocFile
+import knodes
 
 class   Implementation(Grammar, KC_Statement):
     entry = 'Implementation'
@@ -22,7 +23,11 @@ def Impl(self, ast, name, body):
     if hasattr(body, "body") and body.body:
         name = self.value(name)
         for item in body.body:
-            # mangledname = KoocFile.mangled_name_of_symbol(name, item._name, item._ctype.mangle())
-            # print(mangledname)
-            ast.ref.body.append(item)
+            if hasattr(item, "_ctype"):
+                params = ""
+                if isinstance(item._ctype, knodes.KFuncType):
+                     params = item._ctype.mangle_params()
+                mangled_name = KoocFile.mangled_name_of_symbol(name, item._name, item._ctype.mangle(), params)
+                item._name = mangled_name
+                ast.ref.body.append(item)
     return True
