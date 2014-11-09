2587573a689d488632db43750c8b787027d238e7
marie <marie.lefebvre@epitech.eu>
2014-11-09 04:52:51 +0100

    ajout de la fonction module_variables_nodes dans KoocFile pour recuperer toutes les variables du module dans l'implementation

--- KoocFile.py
+++ KoocFile.py
@@ -32,23 +32,31 @@ def register_module_symbol(module_name, symbol_name, symbol_type, mangled_name, 
     content = (mangled_name, assign_node)
     # modules["Moncul"]["tuturu"]["__saisonne__6"] = "poulet"
     # print(modules["Moncul"]["tuturu"]["__saisonne__6"])
-    kfimpl.set_overload_content(module_name, symbol_name, vartype, params_types, symbol_type, content)
+    kfimpl.set_overload_content(module_name, vartype, symbol_name, params_types, symbol_type, content)
 
 
 def inferred_mangled_name_of_symbol(module_name, symbol_name, params_types=""):
     vartype = kfimpl.get_vartype(params_types)
-    params_content = kfimpl.get_params_content(module_name, symbol_name, vartype, params_types)
+    params_content = kfimpl.get_params_content(module_name, vartype, symbol_name, params_types)
     if len(params_content) > 1:
         raise RuntimeError("Ambiguous call of symbol \"" + symbol_name + "\" from the module \"" + module_name + "\"")
     return next(iter(params_content.values()))[0]
 
 def mangled_name_of_symbol(module_name, symbol_name, symbol_type, params_types=""):
     vartype = kfimpl.get_vartype(params_types)
-    return kfimpl.get_overload_content(module_name, symbol_name, vartype, params_types, symbol_type)[0]
+    return kfimpl.get_overload_content(module_name, vartype, symbol_name, params_types, symbol_type)[0]
 def assign_node_of_symbol(module_name, symbol_name, symbol_type, params_types=""):
     vartype = kfimpl.get_vartype(params_types)
-    return kfimpl.get_overload_content(module_name, symbol_name, vartype, params_types, symbol_type)[1]
+    return kfimpl.get_overload_content(module_name, vartype, symbol_name, params_types, symbol_type)[1]
 
+def module_variables_nodes(module_name):
+    variables_nodes = []
+    vartype_content = kfimpl.get_vartype_content(module_name, "variable")
+    for symbol_content in iter(vartype_content.values()):
+        for params_content in iter(symbol_content.values()):
+            for overload_content in iter(params_content.values()):
+                variables_nodes.append(overload_content[1])
+    return variables_nodes
 
 def debugCleanAll():
     imported_file_names.clear()
--- KoocFileImpl.py
+++ KoocFileImpl.py
@@ -5,32 +5,35 @@ class KFImpl():
         self.modules = modules
 
     def get_vartype(self, params_types):
-        vartype = "fun"
+        vartype = "function"
         if params_types == "":
-            vartype = "var"
+            vartype = "variable"
             params_types = "__666__"
         return vartype
     # def get_module_content(self, module_name):
     #     return self.modules[module_name]
 
-    # def get_symbol_content(self, module_name, symbol_name):
-    #     return self.modules[module_name][symbol_name]
-
-    # # vartype = "var" ou "fun"
-    def get_vartype_content(self, module_name, symbol_name, vartype):
-        vartype_content = self.modules[module_name][symbol_name][vartype]
+    # vartype = "var" ou "fun"
+    def get_vartype_content(self, module_name, vartype):
+        vartype_content = self.modules[module_name][vartype]
         if not vartype_content:
-            raise RuntimeError("No definition of symbol \"" + symbol_name + "\" in the module \"" + module_name + "\"")
+            raise RuntimeError("No " + vartype + "s in the module \"" + module_name + "\"")
         return vartype_content
 
-    def get_params_content(self, module_name, symbol_name, vartype, params_types):
-        params_content = self.modules[module_name][symbol_name][vartype][params_types]
+    def get_symbol_content(self, module_name, vartype, symbol_name):
+        symbol_content = self.modules[module_name][vartype][symbol_name]
+        if not symbol_content:
+            raise RuntimeError("No definition of symbol \"" + symbol_name + "\" in the module \"" + module_name + "\"")
+        return symbol_content
+
+    def get_params_content(self, module_name, vartype, symbol_name, params_types):
+        params_content = self.modules[module_name][vartype][symbol_name][params_types]
         if not params_content:
             raise RuntimeError("No definition of symbol \"" + symbol_name + "\" in the module \"" + module_name + "\"")
         return params_content
 
-    def get_overload_content(self, module_name, symbol_name, vartype, params_types, symbol_type):
-        overload_content = self.modules[module_name][symbol_name][vartype][params_types][symbol_type]
+    def get_overload_content(self, module_name, vartype, symbol_name, params_types, symbol_type):
+        overload_content = self.modules[module_name][vartype][symbol_name][params_types][symbol_type]
         if not overload_content:
             raise RuntimeError("No definition of symbol \"" + symbol_name + "\" in the module \"" + module_name + "\"")
         return overload_content
@@ -39,22 +42,22 @@ class KFImpl():
     # def set_module_content(self, module_name, content):
     #     self.modules[module_name] = content
 
-    # def set_symbol_content(self, module_name, symbol_name, content):
-    #     self.modules[module_name][symbol_name] = content
-
     # # vartype = "var" ou "fun"
-    # def set_vartype_content(self, module_name, symbol_name, vartype, content):
-    #     self.modules[module_name][symbol_name][vartype] = content
+    # def set_vartype_content(self, module_name, vartype, content):
+    #     self.modules[module_name][vartype] = content
 
-    # def set_params_content(self, module_name, symbol_name, vartype, params_types, content):
-    #     self.modules[module_name][symbol_name][vartype][params_types] = content
+    # def set_symbol_content(self, module_name, vartype, symbol_name, content):
+    #     self.modules[module_name][vartype][symbol_name] = content
 
-    def set_overload_content(self, module_name, symbol_name, vartype, params_types, symbol_type, content):
-        if self.modules[module_name][symbol_name][vartype][params_types][symbol_type]:
+    # def set_params_content(self, module_name, vartype, symbol_name, params_types, content):
+    #     self.modules[module_name][vartype][symbol_name][params_types] = content
+
+    def set_overload_content(self, module_name, vartype, symbol_name, params_types, symbol_type, content):
+        if self.modules[module_name][vartype][symbol_name][params_types][symbol_type]:
             # print("EEEEEEEEEEERRRRRRRRRRRRRRRRRROOOOOOOOOOOOOOOOOOOOOOOOOOOOOOORRRRRRRRRRRRRRR POURQUOIIII ?")
             # print("params_types = ", params_types)
             # print("symbol_type = ", symbol_type)
             # print("vartype = ", vartype)
             # print("content = ", content)
             raise RuntimeError("Redefinition of symbol \"" + symbol_name + "\" in the module \"" + module_name + "\"")
-        self.modules[module_name][symbol_name][vartype][params_types][symbol_type] = content
+        self.modules[module_name][vartype][symbol_name][params_types][symbol_type] = content
