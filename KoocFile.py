7487bfdbade8a44603db3583b28f5ebbb68f2a07
marie <marie.lefebvre@epitech.eu>
2014-11-11 08:15:00 +0100

    register_class_symbol + rangement de KoocFileImpl

--- KoocFile.py
+++ KoocFile.py
@@ -24,45 +24,25 @@ def register_file(file_name):
 
 nested_dict = lambda: collections.defaultdict(nested_dict)
 modules = nested_dict()
+classes = nested_dict()
 
-kfimpl = KFImpl(imported_file_names, modules)
+kfimpl = KFImpl(imported_file_names, modules, classes)
 
 def register_module(module_name):
     pass
     # if not module_name in modules:
     #     modules[module_name] = {}
 
+# params_types MUST be set to "" for variables
 def register_module_symbol(module_name, symbol_name, params_types, symbol_type, mangled_name, assign_node = None):
-    # print("REGISTER: ", module_name, symbol_name, params_types, symbol_type, mangled_name)
-    vartype = kfimpl.get_vartype(params_types)
-    content = (mangled_name, assign_node)
-    kfimpl.set_overload_content(module_name, vartype, symbol_name, params_types, symbol_type, content)
-
+    kfimpl.register_symbol(modules, module_name, symbol_name, params_types, symbol_type, mangled_name, assign_node)
+# params_types MUST be set to "" for variables
+def register_class_symbol(module_name, symbol_name, params_types, symbol_type, mangled_name, assign_node = None):
+    kfimpl.register_symbol(classes, module_name, symbol_name, params_types, symbol_type, mangled_name, assign_node)
 
 # params_types MUST be set to "" for variables
 def mangled_name_of_symbol(module_name, symbol_name, params_types=None, symbol_type=""):
-    # print("TRY TO GET: ", module_name, symbol_name, params_types, symbol_type)
-    if params_types == None:
-        vartype = "function"
-    else:
-        vartype = kfimpl.get_vartype(params_types)
-
-    symbol_content = kfimpl.get_symbol_content(module_name, vartype, symbol_name)
-
-    if not params_types:
-        kfimpl.check_ambiguous(symbol_content, module_name, symbol_name, vartype)
-        params_content = next(iter(symbol_content.values()))
-    else:
-        params_content = kfimpl.get_params_content(module_name, vartype, symbol_name, params_types)
-
-    if not symbol_type or symbol_type == "":
-        kfimpl.check_ambiguous(params_content, module_name, symbol_name, vartype)
-        overload_content = next(iter(params_content.values()))
-    else:
-        overload_content = kfimpl.get_overload_content(module_name, vartype, symbol_name, params_content, symbol_type)
-
-    # print("OK : ", overload_content[0])
-    return overload_content[0]
+    return kfimpl.mangled_name_of_symbol(modules, module_name, symbol_name, params_types, symbol_type)
 
 # def inferred_mangled_name_of_symbol(module_name, symbol_name, params_types=""):
 #     vartype = kfimpl.get_vartype(params_types)
@@ -76,14 +56,14 @@ def mangled_name_of_symbol(module_name, symbol_name, params_types=None, symbol_t
 #     vartype = kfimpl.get_vartype(params_types)
 #     return kfimpl.get_overload_content(module_name, vartype, symbol_name, params_types, symbol_type)[0]
 
-# def assign_node_of_symbol(module_name, symbol_name, symbol_type, params_types=""):
-#     vartype = kfimpl.get_vartype(params_types)
-#     return kfimpl.get_overload_content(module_name, vartype, symbol_name, params_types, symbol_type)[1]
+# params_types MUST be set to "" for variables
+def assign_node_of_symbol(module_name, symbol_name, params_types, symbol_type):
+    return kfimpl.assign_node_of_symbol(modules, module_name, symbol_name, params_types, symbol_type)
 
 def module_variables_nodes(module_name):
     variables_nodes = []
     try:
-        vartype_content = kfimpl.get_vartype_content(module_name, "variable")
+        vartype_content = kfimpl.get_vartype_content(modules, module_name, "variable")
     except RuntimeError:
         return variables_nodes
     for symbol_content in iter(vartype_content.values()):
@@ -95,4 +75,5 @@ def module_variables_nodes(module_name):
 def debugCleanAll():
     imported_file_names.clear()
     modules.clear()
-    kfimpl = KFImpl(imported_file_names, modules)
+    classes.clear()
+    kfimpl = KFImpl(imported_file_names, modules, classes)
--- KoocFileImpl.py
+++ KoocFileImpl.py
@@ -1,10 +1,15 @@
 
 class KFImpl():
-    def __init__(self, imported_file_names, modules):
+    def __init__(self, imported_file_names, modules, classes):
         self.imported_file_names = imported_file_names
         self.modules = modules
+        self.classes = classes
 
 # TODO : raise KoocException instead of RuntimeError
+
+###########
+## UTILS ##
+###########
 
     def check_no_definition(self, content, module_name, symbol_name, vartype):
         if not content:
@@ -15,7 +20,6 @@ class KFImpl():
         if len(content) > 1:
             raise RuntimeError("Ambiguous call of " + vartype + " \"" + symbol_name + "\" from the module \"" + module_name + "\"")
 
-
     def get_vartype(self, params_types):
         if params_types == "":
             vartype = "variable"
@@ -24,31 +28,73 @@ class KFImpl():
             vartype = "function"
         return vartype
     # def get_module_content(self, module_name):
-    #     return self.modules[module_name]
+    #     return mc[module_name]
 
-    def get_vartype_content(self, module_name, vartype):
-        vartype_content = self.modules[module_name][vartype]
+#############
+## SETTERS ##
+#############
+
+    def register_symbol(self, mc, module_name, symbol_name, params_types, symbol_type, mangled_name, assign_node = None):
+        # print("REGISTER: ", module_name, symbol_name, params_types, symbol_type, mangled_name)
+        vartype = self.get_vartype(params_types)
+        content = (mangled_name, assign_node)
+        self.set_overload_content(mc, module_name, vartype, symbol_name, params_types, symbol_type, content)
+
+    def set_overload_content(self, mc, module_name, vartype, symbol_name, params_types, symbol_type, content):
+        if mc[module_name][vartype][symbol_name][params_types][symbol_type]:
+            raise RuntimeError("Redeclaration of " + vartype + " \"" + symbol_name + "\" in the module \"" + module_name + "\"")
+        mc[module_name][vartype][symbol_name][params_types][symbol_type] = content
+
+
+#############
+## GETTERS ##
+#############
+
+    def get_vartype_content(self, mc, module_name, vartype):
+        vartype_content = mc[module_name][vartype]
         if not vartype_content:
             raise RuntimeError("No " + vartype + "s in the module \"" + module_name + "\"")
         return vartype_content
 
-    def get_symbol_content(self, module_name, vartype, symbol_name):
-        symbol_content = self.modules[module_name][vartype][symbol_name]
+    def get_symbol_content(self, mc, module_name, vartype, symbol_name):
+        symbol_content = mc[module_name][vartype][symbol_name]
         return self.check_no_definition(symbol_content, module_name, symbol_name, vartype)
 
-    def get_params_content(self, module_name, vartype, symbol_name, params_types):
-        params_content = self.modules[module_name][vartype][symbol_name][params_types]
+    def get_params_content(self, mc, module_name, vartype, symbol_name, params_types):
+        params_content = mc[module_name][vartype][symbol_name][params_types]
         return self.check_no_definition(params_content, module_name, symbol_name, vartype)
 
-    def get_overload_content(self, module_name, vartype, symbol_name, params_types, symbol_type):
+    def get_overload_content(self, mc, module_name, vartype, symbol_name, params_types, symbol_type):
         if type(params_types) != str:
             overload_content = params_types[symbol_type]# params_types est ici params_content
         else:
-            overload_content = self.modules[module_name][vartype][symbol_name][params_types][symbol_type]
+            overload_content = mc[module_name][vartype][symbol_name][params_types][symbol_type]
         return self.check_no_definition(overload_content, module_name, symbol_name, vartype)
 
+    def mangled_name_of_symbol(self, mc, module_name, symbol_name, params_types=None, symbol_type=""):
+        # print("TRY TO GET: ", module_name, symbol_name, params_types, symbol_type)
+        if params_types == None:
+            vartype = "function"
+        else:
+            vartype = self.get_vartype(params_types)
+    
+        symbol_content = self.get_symbol_content(mc, module_name, vartype, symbol_name)
+    
+        if not params_types:
+            self.check_ambiguous(symbol_content, module_name, symbol_name, vartype)
+            params_content = next(iter(symbol_content.values()))
+        else:
+            params_content = self.get_params_content(mc, module_name, vartype, symbol_name, params_types)
+    
+        if not symbol_type or symbol_type == "":
+            self.check_ambiguous(params_content, module_name, symbol_name, vartype)
+            overload_content = next(iter(params_content.values()))
+        else:
+            overload_content = self.get_overload_content(mc, module_name, vartype, symbol_name, params_content, symbol_type)
+    
+        # print("OK : ", overload_content[0])
+        return overload_content[0]
 
-    def set_overload_content(self, module_name, vartype, symbol_name, params_types, symbol_type, content):
-        if self.modules[module_name][vartype][symbol_name][params_types][symbol_type]:
-            raise RuntimeError("Redefinition of " + vartype + " \"" + symbol_name + "\" in the module \"" + module_name + "\"")
-        self.modules[module_name][vartype][symbol_name][params_types][symbol_type] = content
+    def assign_node_of_symbol(self, mc, module_name, symbol_name, params_types, symbol_type):
+        vartype = self.get_vartype(params_types)
+        return self.get_overload_content(module_name, vartype, symbol_name, params_types, symbol_type)[1]
